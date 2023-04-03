0b9b228418d0d2223839ebe2fdbb53efae424e7e
Alex Marques <jalexandrevm@gmail.com>
2023-04-03 11:20:52 -0300

    incluindo arrays com partes da frase

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 function getRandomNumber(num) {
     return Math.floor(Math.random() * num);
 }
+
+const subjects = ['O cachorro', 'A tartaruga', 'O coelho', 'O elefante', 'A girafa', 'O leão', 'A arara'];
+const actions = ['correu', 'pulou', 'dormiu', 'comeu', 'cantou'];
+const hows = ['rapidamente', 'devagar', 'com fome', 'alegremente', 'tristemente'];
