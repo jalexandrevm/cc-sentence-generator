4edaee5bdb2a523f999628a9fff63549f5238cad
Alex Marques <jalexandrevm@gmail.com>
2023-04-03 11:25:41 -0300

    incluindo função para montar a frase usando a função que gera número aleatório

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function getRandomNumber(num) {
 const subjects = ['O cachorro', 'A tartaruga', 'O coelho', 'O elefante', 'A girafa', 'O leão', 'A arara'];
 const actions = ['correu', 'pulou', 'dormiu', 'comeu', 'cantou'];
 const hows = ['rapidamente', 'devagar', 'com fome', 'alegremente', 'tristemente'];
+
+function generateRandomSentence() {
+    const subjectIndex = getRandomNumber(subjects.length);
+    const actionIndex = getRandomNumber(actions.length);
+    const howIndex = getRandomNumber(hows.length);
+
+    const subject = subjects[subjectIndex];
+    const action = actions[actionIndex];
+    const how = hows[howIndex];
+
+    const sentence = `${subject} ${action} ${how}.`;
+
+    return sentence;
+}
